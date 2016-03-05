ab13792bf3cebf36144d75d74e1099d2c23ff1b6
Maximilian Richt <maxi@richt.name>
2016-03-05 17:57:54 +0100

    use github pages path

--- freeze.py
+++ freeze.py
@@ -4,6 +4,8 @@ from flask_frozen import Freezer
 from pulse import app
 from app import models
 
+app.config['FREEZER_BASE_URL'] = 'https://robbi5.github.io/pulse/'
+
 freezer = Freezer(app)
 
 # Currently unused sites:
@@ -12,18 +14,18 @@ freezer = Freezer(app)
 #def agency_files():
 #    for agency in models.Agency.all():
 #        yield 'agency', { 'slug': agency['slug'] }
-#
+
 #@freezer.register_generator
 #def domain_files():
 #    for domain in models.Domain.all():
 #        yield 'domain', { 'hostname': domain['domain'] }
 
 @freezer.register_generator
-def data_files():
-    yield '/data/reports/https.json'
-    yield '/data/domains/https.json'
-    yield '/data/domains/https.csv'
-    yield '/data/agencies/https.json'
+def report_files():
+    yield 'report', { 'report_name': 'https' }
+    yield 'agency_report', { 'report_name': 'https' }
+    yield 'domain_report', { 'report_name': 'https', 'ext': 'json' }
+    yield 'domain_report', { 'report_name': 'https', 'ext': 'csv' }
 
 if __name__ == '__main__':
     freezer.freeze()
