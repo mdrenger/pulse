887a0bbdf3eb5d99cfe10d9e64a6ff898b563911
Maximilian Richt <maxi@richt.name>
2016-03-05 16:51:41 +0100

    first static build

---
+++ static/js/https/agencies.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+
+  $.get("/data/agencies/https.json", function(data) {
+    renderTable(data.data);
+  });
+
+  var percentBar = function(field) {
+    return function(data, type, row) {
+      if (type == "sort")
+        return null;
+      return Utils.progressBar(Utils.percent(
+        row.https[field], row.https.eligible
+      ));
+    };
+  }
+
+  var renderTable = function(data) {
+    $("table").DataTable({
+      responsive: true,
+      initComplete: Utils.searchLinks,
+
+      data: data,
+
+      columns: [
+        {data: "name"},
+        {
+          data: "https.eligible",
+          render: Utils.filterAgency("https")
+        },
+        {data: "https.uses"},
+        {data: "https.enforces"},
+        {data: "https.hsts"},
+        {data: "https.grade"}
+      ],
+
+      // order by number of domains
+      order: [[1, "desc"]],
+
+      columnDefs: [
+        {
+          targets: 0,
+          cellType: "td",
+          createdCell: function (td) {
+            td.scope = "row";
+          }
+        },
+        {
+          render: percentBar("uses"),
+          targets: 2,
+        },
+        {
+          render: percentBar("enforces"),
+          targets: 3,
+        },
+        {
+          render: percentBar("hsts"),
+          targets: 4,
+        },
+        {
+          render: percentBar("grade"),
+          targets: 5,
+        }
+      ],
+
+      "oLanguage": {
+        "oPaginate": {
+          "sPrevious": "<<",
+          "sNext": ">>"
+        }
+      },
+
+      dom: 'Lftrip'
+
+    });
+  };
+
+});
---
+++ static/js/utils.js
@@ -0,0 +1,52 @@
+
+var Utils = {
+  // ignores the 'type' and 'row' args if sent as datatables callback
+
+  percent: function(num, denom) {
+    return Math.round((num / denom) * 100);
+  },
+
+  progressBar: function(data) {
+
+    return '' +
+      '<div class="progress-bar-indication">' +
+        '<span class="meter width' + data + '" style="width: ' + data + '%">' +
+          '<p>' + data + '%</p>' +
+        '</span>' +
+      '</div>';
+  },
+
+  linkDomain: function(data, type, row) {
+    if (type == "sort")
+      return data;
+    else
+      return "" +
+        "<a href=\"" + row['canonical'] + "\" target=\"blank\">" +
+          data +
+        "</a>";
+  },
+
+  // used to make "71" say "71 domains" and link to filtered domains
+  filterAgency: function(page) {
+    return function(data, type, row) {
+      if (type == "sort")
+        return data;
+      else
+        return "" +
+          "<a href=\"/" + page + "/domains/#" +
+            QueryString.stringify({q: row["name"]}) + "\">" +
+            data +
+          "</a>";
+    }
+  },
+
+  searchLinks: function() {
+    var api = this.api();
+    var query = QueryString.parse(location.hash).q;
+
+    if (query) {
+      $("input[type=search]").val(query);
+      api.search(query).draw();
+    }
+  }
+};
