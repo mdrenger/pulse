b6e9a6463a29d0a98eb2b5c781efc074acb18086
Maximilian Richt <maxi@richt.name>
2018-08-25 19:29:34 +0200

    agencies: link to matching domain type search

--- static/js/https/agencies.js
+++ static/js/https/agencies.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
+  var domain_type = $('script[data-domain-type]').attr('data-domain-type');
 
   var report_url = $('script[data-agency-report-url]').attr('data-agency-report-url');
+  var report_csv_url = $('script[data-agency-report-csv-url]').attr('data-agency-report-csv-url');
+
   $.get(report_url, function(data) {
     Tables.initAgency(data.data, {
 
-      csv: "/data/hosts/https.csv",
+      csv: report_csv_url,
 
       columns: [
         {
@@ -56,7 +59,7 @@ $(document).ready(function () {
 
     var link = function(text) {
       return "" +
-        "<a href=\"/https/domains/#" +
+        "<a href=\"/https/" + domain_type + "/domains/#" +
           QueryString.stringify({q: row["name"]}) + "\">" +
            text +
         "</a>";
